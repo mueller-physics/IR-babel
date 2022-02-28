3bb057aaaa4e9419083f958cd76948f4823927bc
Marcel Müller <muellerphysics@gmail.com>
2022-03-01 00:18:40 +0100

    Updated IRMP library version

--- IR-babel.ino
+++ IR-babel.ino
@@ -1,18 +1,28 @@
 #include <Arduino.h>
+
+
 #define IRSND_IR_FREQUENCY          38000
+
+
+#ifdef ARDUINO_AVR_ATTINYX5
 #define IRSND_OUTPUT_PIN 6
 #define IRMP_INPUT_PIN 8
-//#define DECODE_RC5
+#else
+#define IRSND_OUTPUT_PIN 2
+#define IRMP_INPUT_PIN 0
+#define IRMP_FEEDBACK_LED_PIN 1
+#endif
+
 #define USE_ONE_TIMER_FOR_IRMP_AND_IRSND
+#define IRMP_SUPPORT_NEC_PROTOCOL 1
 
-
-#include <irmpSelectMain15Protocols.h>
-#include <irsndSelectMain15Protocols.h>
-#include <irsnd.c.h>
-#include <irmp.c.h>
+#include <irmp.hpp>
+#include <irsnd.hpp>
 
 IRMP_DATA irsnd_data;
 IRMP_DATA irmp_data;
+
+uint8_t mute_count;
 
 void setup() {
     // put your setup code here, to run once:
@@ -21,8 +31,11 @@ void setup() {
     irmp_init();
     irsnd_init();
     irmp_irsnd_LEDFeedback(true); 
-}
 
+	Serial.println(F("IR babel ready"));
+
+	mute_count=0;
+}
 
 
 void loop() {
@@ -34,38 +47,60 @@ void loop() {
 
     if (irmp_get_data(&irmp_data)) {
 
-	Serial.println( irmp_data.address );
-	Serial.println( irmp_data.command );
+	Serial.print(F("0x"));
+	Serial.println( irmp_data.address, HEX );
+	Serial.print(F("0x"));
+	Serial.println( irmp_data.command, HEX );
 	Serial.println( "---" );
 
 
 	if ( irmp_data.address == 0x0001 ) {
-	    switch (irmp_data.command) {
-
+	    
+		switch (irmp_data.command) {
+		
 		case 0x10:
-		    Serial.println(" vol UP");
+		    Serial.println(F(" vol UP"));
 		    irsnd_data.command = 0x19;		
 		    irsnd_send_data(&irsnd_data, true);
 		    delay(100);
+			mute_count=0;
 		    break;
 
 		case 0x11:
-		    Serial.println(" vol DN");
+		    Serial.println(F(" vol DN"));
 		    irsnd_data.command = 0x1c;		
 		    irsnd_send_data(&irsnd_data, true);
 		    delay(100);
+			mute_count=0;
 		    break;
 
 		case 0x0d:
-		    Serial.println(" vol mute");
-		    irsnd_data.command = 0xb8;		
-		    irsnd_send_data(&irsnd_data, true);
-		    delay(100);
+			//Serial.print("mute: ");
+			//Serial.println(mute_count);
+			if (mute_count++<2) {
+				// pressing mute once or twice just sends a mute command
+		    	Serial.println(F(" vol mute"));
+		    	irsnd_data.command = 0x47;		
+			    irsnd_send_data(&irsnd_data, true);
+		    	delay(100);
+			} else {
+				// pressing mute three times sends 'power' followed by 'select optical input'
+		    	Serial.println(F(" vol mute 3x --> power + optical input"));
+		    	irsnd_data.command = 0x45;		
+			    irsnd_send_data(&irsnd_data, true); // send the 'power' command
+				// especially if set to 'bluetooth', there needs to be some delay 
+				// before the soundbar accepts an input switch command
+		    	delay(2500);						
+				irsnd_data.command = 0x40;	// switch to optical (see readme for command list)
+			    irsnd_send_data(&irsnd_data, true);
+		    	delay(200);
+				mute_count=0;
+			}
 		    break;
 
 	    }
 	}
 
-    }
+}
 
 }
--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 Make the Fire TV remote talk to a [Renkforce](https://www.conrad.com/p/renkforce-tb230-soundbar-black-bluetooth-nfc-wall-brackets-1384497)
 sound bar that uses weird(?) IR codes.
 
+Command list:
+
+NEC protocol, address 0xff00
+
+- 0x45  power
+- 0x47  mute
+- 0x40  select optical input
+- 0x07  select aux 1 input
+- 0x15  select aux 2 input
+- 0x09  select bluetooth input
+- 0x19  volume up
+- 0x1c  volume down
+- 0x18  play/pause
+- 0x0c  back / last track
+- 0x5e  forward / next track
+- 0x42  sound profile 'flat'
+- 0x44  sound profile 'movie'
+- 0x4a  soudn profile 'music'
+
+
